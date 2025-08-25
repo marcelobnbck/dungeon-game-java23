7ebfb07f88cd7d7e7342a417781de9fe1a6ee2de
Marcelo Beck <marcelobck@gmail.com>
2025-08-24 23:57:26 -0300

    adding rest interface

--- src/main/java/org/example/dungeonrescue/controller/DungeonController.java
+++ src/main/java/org/example/dungeonrescue/controller/DungeonController.java
@@ -1,5 +1,6 @@
 package org.example.dungeonrescue.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,5 +13,33 @@ public class DungeonController {
     // DTO for response
     public record DungeonResponse(int minInitialHealth) {}
 
+    @PostMapping("/min-health")
+    @ResponseStatus(HttpStatus.OK)
+    public DungeonResponse calculateMinHealth(@RequestBody DungeonRequest request) {
+        int result = calculateMinimumHP(request.dungeon());
+        return new DungeonResponse(result);
+    }
+
+    private int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[] dp = new int[n + 1];
+        final int INF = Integer.MAX_VALUE;
+
+        for (int j = 0; j <= n; j++) {
+            dp[j] = INF;
+        }
+        dp[n - 1] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            dp[n] = INF;
+            for (int j = n - 1; j >= 0; j--) {
+                int needNext = Math.min(dp[j], dp[j + 1]);
+                int needHere = needNext - dungeon[i][j];
+                dp[j] = Math.max(1, needHere);
+            }
+        }
+        return dp[0];
+    }
 }
 
